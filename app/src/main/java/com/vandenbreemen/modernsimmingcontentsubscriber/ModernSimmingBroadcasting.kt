e9eab1ecc01b86d84c0963d9d53be1548b14c5e1
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-10-08 21:39:55 -0400

    Begin work defining broadcasting code for use on client/main app tiers

---
+++ app/src/main/java/com/vandenbreemen/modernsimmingcontentsubscriber/ModernSimmingBroadcasting.kt
@@ -0,0 +1,16 @@
+package com.vandenbreemen.modernsimmingcontentsubscriber
+
+/**
+ * Specialist for handling broadcasts to and from the client
+ */
+class ModernSimmingBroadcasting {
+
+    companion object {
+        const val NEW_POST_IN_GROUP = "NewPostInGroup"
+        const val PARAM_GROUP_NAME = "groupName"
+        const val PARAM_TTS_CURRENT_POSITION = "position"
+        const val PARAM_TTS_TOTAL_STRINGS_TO_SPEAK = "totalStringsToSpeak"
+        const val TTS_SEEK_TO = "TTSSeekTo"
+    }
+
+}
