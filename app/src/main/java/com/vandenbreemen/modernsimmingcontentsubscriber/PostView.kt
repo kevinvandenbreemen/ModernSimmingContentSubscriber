835e409614d2eeeda26af1d4c7a295f8651265e0
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-10-17 14:36:33 -0400

    Flag for indicating a post as selected

--- app/src/main/java/com/vandenbreemen/modernsimmingcontentsubscriber/PostView.kt
+++ app/src/main/java/com/vandenbreemen/modernsimmingcontentsubscriber/PostView.kt
@@ -14,4 +14,11 @@ data class PostView(
     val url: String,
 
     val content: String
-)
+) {
+
+    /**
+     * Indicate that an app (such as a playback list) has selected this post
+     */
+    var selected: Boolean = false
+
+}
